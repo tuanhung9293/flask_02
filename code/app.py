19ea3e51e47841172d9872de67f0c7dc5b156837
tony-ho <tony.ho@idemlabs.com>
2019-10-23 10:34:28 +0700

    update

--- code/app.py
+++ code/app.py
@@ -1,8 +1,9 @@
-from flask import Flask, request
-from flask_restful import Resource, Api, reqparse
-from flask_jwt import JWT, jwt_required
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
 from security import authenticate, identify
 from user2 import UserRegister
+from item import Item, Items
 
 app = Flask(__name__)
 app.secret_key = 'tony 111'
@@ -11,50 +12,10 @@ api = Api(app)
 jwt = JWT(app, authenticate, identify)
 items = []
 
-class Item(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('price',
-        type=float,
-        required=True,
-        help='This field cannot be left blank!'
-    )
-
-    @jwt_required()
-    def get(self, name):
-        item = next(iter([ x for x in items if x['name'] == name]), None)
-        return {'item': item }, 200 if item else 404
-
-    def post(self, name):
-        if next(iter([x for x in items if x['name'] == name]), None):
-            return {'message': "An item with name '{}' already exists".format(name)}, 400
-
-        data = Item.parser.parse_args()
-        item = {'name': name, 'price': data['price']}
-        items.append(item)
-        return item, 201
-
-    def delete(self, name):
-        global items
-        items = [x for x in items if x['name'] != name]
-        return {'message': 'Item deleted'}
-
-    def put(self, name):
-        data = Item.parser.parse_args()
-        item = next(iter([ x for x in items if x['name'] == name]), None)
-        if item is None:
-            item = {'name': name, 'price': data['price']}
-            items.append(item)
-        else:
-            item.update(data)
-        return item
-
-
-class Items(Resource):
-    def get(self):
-        return {'items': items}
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(Items, '/items')
 api.add_resource(UserRegister, '/register')
 
-app.run(port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
--- code/create_tables.py
+++ code/create_tables.py
@@ -6,5 +6,8 @@ cursor = connection.cursor()
 create_tables = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
 cursor.execute(create_tables)
 
+create_tables = "CREATE TABLE IF NOT EXISTS items (name text, price real)"
+cursor.execute(create_tables)
+
 connection.commit()
 connection.close()

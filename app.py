d256063a07612bace83fd556409130191f7d3ae8
tony-ho <tony.ho@idemlabs.com>
2019-11-03 11:31:57 +0700

    add postgres

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -7,7 +8,7 @@ from resources.item import Item, Items
 from resources.store import Store, Stores
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'tony 111'
 api = Api(app)
